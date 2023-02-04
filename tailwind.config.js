e3d632fabf0d26935d4e40997ed603a347d4cf37
IchwanSetia <115933434+IchwanSetia@users.noreply.github.com>
2023-02-05 06:39:09 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF994F',
+        secondary: '#FFFAF2',
+        header: '#F6F9FC',
+        footer: '#281900',
+        fontColor: '#636363',
+        three: '#F7FBFA',
+      },
+    },
+  },
+};
